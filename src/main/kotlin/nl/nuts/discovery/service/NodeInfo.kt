3cd348872f7cc8e1fae9b372d5275ef8a9cf6de5
stevenvegt <steven.vandervegt@nedap.com>
2019-11-25 13:14:18 +0100

    Fix serialization error
    NodeInfo should not be serialized.

--- src/main/kotlin/nl/nuts/discovery/service/NodeInfo.kt
+++ src/main/kotlin/nl/nuts/discovery/service/NodeInfo.kt
@@ -1,10 +1,11 @@
 package nl.nuts.discovery.service
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.NetworkHostAndPort
 
-data class NodeInfo(val nodeInfo: net.corda.core.node.NodeInfo) {
+data class NodeInfo(@JsonIgnore val nodeInfo: net.corda.core.node.NodeInfo) {
 
     @JsonProperty
     fun legalName(): CordaX500Name {
